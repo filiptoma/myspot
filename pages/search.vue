09e64ba0af11fc980216c2ec078dafb6efcf4da0
Filip Toma <filiptoma@hotmail.com>
2020-06-20 10:46:46 +0200

    testing horizontal scroll functionality with disabled scrollbar

--- pages/search.vue
+++ pages/search.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <h1>search page</h1>
+        <div class="overflow-x-scroll w-screen scrolling-touch overflow-x-hidden test2">
+<div class="test flex flex-wrap">
+            <h1>c oeeeeeeeee </h1>
+            <h1>co eeeeeeeee </h1>
+            <h1>coe eeeeeeee </h1>
+            <h1>coee eeeeeee </h1>
+            <h1>coeee eeeeee </h1>
+            <h1>coeeee eeeee </h1>
+            <h1>coeeeee eeee </h1>
+            <h1>coeeeeee eee </h1>
+            <h1>coeeeeeee ee </h1>
+            <h1>coeeeeeeee e </h1>
+        </div>
+        </div>
+        
         <Footer />
     </div>
 </template>
@@ -23,5 +38,13 @@ export default {
 </script>
 
 <style scoped>
-
+.test {
+    width: 200vw;
+}
+.test2::-webkit-scrollbar {
+    display: none;
+}
+.test2 {
+    -ms-overflow-style: none;
+}
 </style>
